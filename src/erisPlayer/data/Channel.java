ed0da4199e6155b1365ec2649f9d2e719487c196
Eris Television <eris-television@proton.me>
2022-08-26 02:27:27 +0200

    Add Channel-DataClass

---
+++ src/erisPlayer/data/Channel.java
@@ -0,0 +1,40 @@
+package erisPlayer.data;
+
+import java.util.ArrayList;
+
+public class Chanal {
+	
+	private final String name;
+	private final String chanalID;
+	private final String tag;
+	
+	private ArrayList<Video> videoList;
+	
+	public Chanal(String name, String chanalID, String tag) {
+		this.name = name;
+		this.chanalID = chanalID;
+		this.tag = tag;
+		this.videoList = new ArrayList<>();
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getChanalID() {
+		return chanalID;
+	}
+
+	public String getTag() {
+		return tag;
+	}
+	
+	public void addVideo(Video video) {
+		videoList.add(video);
+	}
+	
+	public ArrayList<Video> getVideoList() {
+		return videoList;
+	}
+	
+}
