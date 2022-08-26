e4c1338d49d0c689f5f6804df958c8fa08d598be
Eris Television <eris-television@proton.me>
2022-08-26 02:59:28 +0200

    Adding ContentLoader-Class

---
+++ src/erisPlayer/data/ContentLoader.java
@@ -0,0 +1,45 @@
+package erisPlayer.data;
+
+import java.util.ArrayList;
+
+import erisPlayer.ErisLogger;
+
+public class ContentLoader {
+	
+	private final String resourcesDir;
+	private final ErisLogger logger;
+	
+	private ArrayList<Channel> channelList;
+	
+	public ContentLoader(String localDir, ErisLogger logger) {
+		this.resourcesDir = localDir + "resources/";
+		this.logger = logger;
+		this.channelList = new ArrayList<>();
+		
+		loadContent();
+	}
+	
+	
+	private void loadContent() {
+		
+		logger.print("Loading Channels ...");
+		
+		
+		logger.printSubline(channelList.size() + " Channels found.");
+		
+		
+		logger.print("Loading Videos ...");
+		for(Channel currentChannel : channelList) {
+			loadVideos(currentChannel);
+			logger.printSubline(currentChannel.getName() +" : "+ currentChannel.getVideoList().size() + " Videos found.");
+		}
+	}
+	
+	private void loadVideos(Channel channel) {
+		
+	}
+	
+	
+	
+	
+}
