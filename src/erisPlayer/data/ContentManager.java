4a9a3349c40aba87125c74d8c2d277b03e21bb93
Eris Television <eris-television@proton.me>
2022-08-27 03:52:00 +0200

    ContentManager adding load and save function

--- src/erisPlayer/data/ContentManager.java
+++ src/erisPlayer/data/ContentManager.java
@@ -1,5 +1,52 @@
 package erisPlayer.data;
 
-public class ContentManager {
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.URI;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 
+import erisPlayer.ErisLogger;
+
+public class ContentManager {
+	
+	private String resourceDir;
+	
+	private ErisLogger logger;
+	
+	private ArrayList<Channel> channelList;
+	public ArrayList<Channel> getChannelList() { return channelList; }
+	public void setChannelList(ArrayList<Channel> channelList) { this.channelList = channelList; }
+
+	public ContentManager(String resourceDir, ErisLogger logger) {
+		this.resourceDir = resourceDir;
+		this.logger = logger;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public void loadContent() {
+		String path = resourceDir + "channels.eris";
+		
+		try(FileInputStream fis = new FileInputStream(Paths.get(new URI(path)).toFile());
+				ObjectInputStream ois = new ObjectInputStream(fis)) {
+			channelList = (ArrayList<Channel>) ois.readObject();
+		} catch (Exception e) {
+			logger.printError("Can't read Channels from File!", e);
+			channelList = new ArrayList<>();
+		}
+	}
+	
+	public void saveContent() {
+		String path = resourceDir + "channels.eris";
+		
+		try(FileOutputStream fos = new FileOutputStream(Paths.get(new URI(path)).toFile());
+				ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+			oos.writeObject(channelList);
+		} catch (Exception e) {
+			logger.printError("Can't write Channels to File!", e);
+		}
+	}
+	
 }
