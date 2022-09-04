25659c4f0d66cacf54cdea0269c4a34217987d8e
Eris Television <eris-television@proton.me>
2022-09-04 23:55:55 +0200

    Altering DownloadManager

--- src/erisPlayer/data/DownloadManager.java
+++ src/erisPlayer/data/DownloadManager.java
@@ -7,35 +7,54 @@ import com.sapher.youtubedl.YoutubeDLResponse;
 
 public class DownloadManager {
 	
+	private String directory;
+	
+	public DownloadManager(String localDir) {
+		this.directory = localDir +"";
+	}
+	
+	public void getNewChannelDownloads(Channel channel) {
+		String videoUrl = "https://www.youtube.com/channel/" + channel.getChanalID();
+		String outputUrl = channel.getTag() + "_%(upload_date)s_%(title)s.%(ext)s";
+		
+		
+		
+		YoutubeDLRequest request = new YoutubeDLRequest(videoUrl, directory);
+		request.setOption("ignore-errors");
+		request.setOption("f", "best");
+		request.setOption("dateafter", 20220830);
+	}
+	
+	public String getDate(Channel channel) {
+		return channel.getLastUpload().getUploadDate().toString();
+	}
 	
 	
 	
 	
 	// Test-Main
-	
 	private void main() throws YoutubeDLException {
 		// Video url to download
-		String videoUrl = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
-
-		// Destination directory
-		String directory = "D:\\";
+		String videoUrl = "https://www.youtube.com/channel/UC0RBJwg8ZfRM8TLGOKTpArA";
 
 		// Build request
 		YoutubeDLRequest request = new YoutubeDLRequest(videoUrl, directory);
 		request.setOption("ignore-errors");		// --ignore-errors
-		request.setOption("output", "%(id)s");	// --output "%(id)s"
-		request.setOption("retries", 10);		// --retries 10
-
+		request.setOption("f", "best");
+		request.setOption("dateafter", 20220830);
+		request.setOption("output", "HRZ_%(upload_date)s_%(title)s.%(ext)s");	// --output "%(id)s"
+		
+		
 		// Make request and return response
 		YoutubeDLResponse response = YoutubeDL.execute(request);
-
+		System.out.println("request end");
 		// Response
 		String stdOut = response.getOut(); // Executable output
 		System.out.println(stdOut);
 	}
 	
 	public static void main(String[] args) throws YoutubeDLException {
-		DownloadManager run = new DownloadManager();
+		DownloadManager run = new DownloadManager("D:\\");
 		run.main();
 	}
 	
