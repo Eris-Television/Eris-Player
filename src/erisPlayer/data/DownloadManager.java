6cf18e9de11162e1610d891a17f795789e8c850c
Eris Television <eris-television@proton.me>
2022-09-07 00:31:04 +0200

    Adding DownloadManager-class

--- src/erisPlayer/data/DownloadManager.java
+++ src/erisPlayer/data/DownloadManager.java
@@ -5,34 +5,65 @@ import com.sapher.youtubedl.YoutubeDLException;
 import com.sapher.youtubedl.YoutubeDLRequest;
 import com.sapher.youtubedl.YoutubeDLResponse;
 
+import erisPlayer.ErisLogger;
+
 public class DownloadManager {
 	
 	private String directory;
 	
-	public DownloadManager(String localDir) {
-		this.directory = localDir +"";
+	private ErisLogger logger;
+	
+	public DownloadManager(String resourceDir, ErisLogger logger) {
+		this.directory = resourceDir +"Downloads/";
+		this.logger = logger;
 	}
 	
-	public void getNewChannelDownloads(Channel channel) {
+	public void downloadNewVideos(Channel channel) {
 		String videoUrl = "https://www.youtube.com/channel/" + channel.getChanalID();
-		String outputUrl = channel.getTag() + "_%(upload_date)s_%(title)s.%(ext)s";
-		
-		
+		String output = channel.getTag() + "_%(upload_date)s_%(title)s.%(ext)s";
 		
 		YoutubeDLRequest request = new YoutubeDLRequest(videoUrl, directory);
 		request.setOption("ignore-errors");
 		request.setOption("f", "best");
-		request.setOption("dateafter", 20220830);
+		request.setOption("dateafter", getDate(channel));
+		request.setOption("output", output);
+		
+		
+		try {
+			// Make request and return response
+			YoutubeDLResponse response = YoutubeDL.execute(request);
+			System.out.println("request end");
+			// Response
+			String stdOut = response.getOut(); // Executable output
+			System.out.println(stdOut);
+		} catch (YoutubeDLException e) {
+			logger.printError("DownloadManger, can't download : " + channel.getChanalID() + "[%s]" + channel.getChanalID(), e);
+		}
+		
 	}
 	
 	public String getDate(Channel channel) {
-		return channel.getLastUpload().getUploadDate().toString();
+		try {
+			return channel.getLastUpload().getUploadDate().toString();
+		}catch (IndexOutOfBoundsException e) {
+			return "20160101";
+		}
 	}
 	
 	
+	public void debug(Channel channel) {
+		System.out.println("DownloadMangaer : Debug ...");
+		
+		String videoUrl = "https://www.youtube.com/channel/" + channel.getChanalID();
+		String output = channel.getTag() + "_%(upload_date)s_%(title)s.%(ext)s";
+		
+		System.out.println(videoUrl);
+		System.out.println(output);
+		System.out.println(directory);
+		System.out.println(getDate(channel));
+	}
 	
-	
-	// Test-Main
+	/*/ Test-Main
 	private void main() throws YoutubeDLException {
 		// Video url to download
 		String videoUrl = "https://www.youtube.com/channel/UC0RBJwg8ZfRM8TLGOKTpArA";
@@ -54,9 +85,9 @@ public class DownloadManager {
 	}
 	
 	public static void main(String[] args) throws YoutubeDLException {
-		DownloadManager run = new DownloadManager("D:\\");
+		DownloadManager run = new DownloadManager("D:\\", new ErisLogger(""));
 		run.main();
-	}
+	}*/
 	
 	
 }
