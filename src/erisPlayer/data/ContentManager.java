5ff46c41e87b4d60d3833a2cae43a74c20efdd8d
Eris Television <eris-television@proton.me>
2022-10-09 23:31:08 +0200

    Extracting into ChannelMethodes

--- src/erisPlayer/data/ContentManager.java
+++ src/erisPlayer/data/ContentManager.java
@@ -13,15 +13,14 @@ import java.util.ArrayList;
 
 import erisPlayer.ErisLogger;
 
-public class ContentManager {
+public class ContentManager extends ChannelMethodes{
 	
 	private String resourceDir;
 	
 	private ErisLogger logger;
 	private DownloadManager downloadManager;
 	
-	private ArrayList<Channel> channelList;
-	public ArrayList<Channel> getChannelList() { return channelList; }
+	
 	public void setChannelList(ArrayList<Channel> channelList) { this.channelList = channelList; }
 
 	public ContentManager(String resourceDir, ErisLogger logger) {
@@ -64,38 +63,6 @@ public class ContentManager {
         }
     }
 
-    /* --- Add / Edit / Remove --- */
-
-    public void addChannel(Channel channel) {
-        channelList.add(channel);
-    }
-
-    public void editChannel(int chnnelIndex, Channel channel) {
-        channelList.set(chnnelIndex, channel);
-    }
-
-    public void removeChannel(int chnnelIndex) {
-        channelList.remove(chnnelIndex);
-    }
-
-    public void addVideo(int channelIndex, Video video) {
-        channelList.get(channelIndex).addVideo(video);
-    }
-
-    public void editVideo(int channelIndex, int videoIndex, Video video) {
-        channelList.get(channelIndex).editVideo(videoIndex, video);
-    }
-
-    public void removeVideo(int channelIndex, int videoIndex) {
-        channelList.get(channelIndex).removeVideo(videoIndex);
-    }
-
-    /* --- Methods --- */
-
-    public Video getLastUnpublishedVideo(Channel channel) {
-        return channel.getLastUnpublishedVideo();
-    }
-    
     /* --- Update Channels --- */
 
     public void updateChannels() {
