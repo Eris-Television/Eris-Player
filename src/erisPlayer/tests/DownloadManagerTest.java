751ab2909fefc1f922c25919d8902ec01851e10f
Eris Television <eris-television@proton.me>
2022-10-10 23:25:20 +0200

    DownloadManagerTest adding dummy constructor

--- src/erisPlayer/tests/DownloadManagerTest.java
+++ src/erisPlayer/tests/DownloadManagerTest.java
@@ -4,18 +4,31 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import erisPlayer.ErisLogger;
 import erisPlayer.data.Channel;
 
 class DownloadManagerTest {
 	
+	private final String directory;
+	private ErisLogger logger;
+	
 	private Channel testChannel = new Channel("Eris Debug", "UCYGFAov8c5mIyKnoGu-JWng", "ERD");
+	
+	/* --- Constructor: --- */
+	
+	public DownloadManagerTest(ErisLogger logger) {
+		directory = "todo";
+		logger = this.logger;
+	}
+	
 	
 	/*
 	 * TODO: Implement:
 	 * 
 	 * 1. Test Download all Videos (No Videos already downloaded)
-	 * 2. Add video and download all Videos (downlaod new videos / update Videos)
+	 * 2. Add video and download all Videos (download new videos / update Videos)
 	 * 
+	 * 3. Adding TestLogger (Dummy not functional) ?
 	 */
 	
 	
