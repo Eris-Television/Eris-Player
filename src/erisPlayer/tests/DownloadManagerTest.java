d45041ab09179f04aabfd0acf0a1b4140313ad80
Eris Television <eris-television@proton.me>
2022-11-20 23:50:33 +0100

    Adding DownloadManagerSpy to DownlaodManagerTests

--- src/erisPlayer/tests/DownloadManagerTest.java
+++ src/erisPlayer/tests/DownloadManagerTest.java
@@ -2,17 +2,17 @@ package erisPlayer.tests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import erisPlayer.ErisLogger;
 import erisPlayer.data.Channel;
-import erisPlayer.data.DownloadManager;
 
 class DownloadManagerTest {
 	
 	private final String directory;
 	private ErisLogger logger;
-	private DownloadManager downlaodManager;
+	private DownloadMangerSpy downlaodManager;
 	private Channel testChannel = new Channel("Eris Debug", "UCYGFAov8c5mIyKnoGu-JWng", "ERD");
 	
 	/* --- Constructor: --- */
@@ -20,12 +20,20 @@ class DownloadManagerTest {
 	public DownloadManagerTest() {
 		directory = "todo";
 		this.logger = new TestLogger(null);
-		this.downlaodManager = new DownloadManager(directory, logger);
+		this.downlaodManager = new DownloadMangerSpy(directory, logger);
+	}
+	
+	@BeforeAll
+	@Test
+	void testCommandLine() {
+		
 	}
 	
 	
 	/*
 	 * TODO: Implement:
+	 * 
+	 * 0. Implement CommandLine-Test
 	 * 
 	 * 1. Test Download all Videos (No Videos already downloaded)
 	 * 2. Add video and download all Videos (download new videos / update Videos)
