bd58be963ebd2673406146c33126e594d820e41a
Eris Television <eris-television@proton.me>
2022-12-21 03:43:28 +0100

    Adding HTML

---
+++ html/script.js
@@ -0,0 +1,51 @@
+
+// --- GLOBAL-Vars : ---
+
+var videoPlayer 
+var videoSource
+
+var webSocket
+
+function onLoad() {
+    videoPlayer = document.getElementById("videoPlayer")
+    videoSource = document.getElementById("videoSource")
+    //openFullscreen();
+
+    webSocket = new WebSocket("127.0.01:2332")
+}
+
+function playNextVideo() {
+    videoSource.src = getNextVideo()
+    videoPlayer.load()
+}
+
+function getNextVideo() {
+    return "t.mp4"
+}
+
+function openFullscreen() {
+
+    var isInFullScreen = (document.fullScreenElement && document.fullScreenElement !==     null) ||    //alternative standard method  
+    (document.mozFullScreen || document.webkitIsFullScreen);
+
+    var docElm = document.documentElement;
+    if (!isInFullScreen) {
+        
+        if (docElm.requestFullscreen) {
+            docElm.requestFullscreen();
+        }
+        else if (docElm.mozRequestFullScreen) {
+            docElm.mozRequestFullScreen();
+            alert("Mozilla entering fullscreen!");
+        }
+        else if (docElm.webkitRequestFullScreen) {
+            docElm.webkitRequestFullScreen();
+            alert("Webkit entering fullscreen!");
+        }
+
+    }
+
+    //var docElm = document.documentElement;
+    //docElm.requestFullscreen();
+    
+}
