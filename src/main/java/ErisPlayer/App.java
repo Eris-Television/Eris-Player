0a743b0de3637adc0743b46f7ae3d5440b1db868
Eris Television <eris-television@proton.me>
2022-12-21 03:25:07 +0100

    Create blank new Maven-Project

---
+++ src/main/java/ErisPlayer/App.java
@@ -0,0 +1,13 @@
+package ErisPlayer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
